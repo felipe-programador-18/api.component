09b6e123ff128435dce15b48c9c35fd3234d5396
Felipe <machadofelipe2016@outlook.com>
2022-04-08 14:48:07 -0300

    c3- adding reactrouterdom

--- apicomponent/src/header.js
+++ apicomponent/src/header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <ul>
-            <li> link 1</li>
-            <li> link 2</li>
+            <li>
+              <Link to='/'> link 1</Link>   </li>
+            <li>
+                <Link to='/2'></Link>
+            </li>
         </ul>
     )
 }
