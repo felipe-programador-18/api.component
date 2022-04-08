d6cb9b9dbf3faa095b4f55191f6304d8d384791a
Felipe <machadofelipe2016@outlook.com>
2022-04-08 15:02:11 -0300

    c7-adding inject dependence

--- apicomponent/src/header.js
+++ apicomponent/src/header.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 
-const Header = () => {
+const Header = ({Link}) => {
     return (
         <ul>
             <li>
               <Link to='/'> link 1</Link>   </li>
             <li>
-                <Link to='/2'></Link>
+                <Link to='/2'>Link2</Link>
             </li>
         </ul>
     )
