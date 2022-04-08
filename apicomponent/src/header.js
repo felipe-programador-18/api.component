90c20c92f3c309196988a32c1f33cdaf5995c4af
Felipe <machadofelipe2016@outlook.com>
2022-04-08 14:33:28 -0300

    c2-create header to inject dependence about react

---
+++ apicomponent/src/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <ul>
+            <li> link 1</li>
+            <li> link 2</li>
+        </ul>
+    )
+}
+
+export default Header
+
+
+
